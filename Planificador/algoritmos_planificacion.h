02e65864f6fdf22f33279aeb29f7b0e8a80ad5b3
Nayla Winter <naylawinter@gmail.com>
2018-04-30 00:07:40 -0300

    Archivo de configuracion de planificador. Estructura Esi. Listas de Esis. Primer boceto de planificador por FIFO.

---
+++ Planificador/algoritmos_planificacion.h
@@ -0,0 +1,14 @@
+/*
+ * algoritmos_planificacion.h
+ *
+ *  Created on: 29 abr. 2018
+ *      Author: utnso
+ */
+
+#ifndef ALGORITMOS_PLANIFICACION_H_
+#define ALGORITMOS_PLANIFICACION_H_
+
+typedef enum {FIFO, SJFsD, SJFcD, HRRN } tipo_algoritmo_planif;
+/* FIFO, SHORT JOB FIRST SIN DESALOJO, SHORT JOB FIRST CON DESALOJO, HRRN*/
+
+#endif /* ALGORITMOS_PLANIFICACION_H_ */
