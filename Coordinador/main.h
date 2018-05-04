92ad8a24a77c373c00f28e932e404c9a3b563c42
Nayla Winter <naylawinter@gmail.com>
2018-05-03 22:17:02 -0300

    Primer boceto instancias/lista instancias

---
+++ Coordinador/main.h
@@ -0,0 +1,30 @@
+/*
+ * main.h
+ *
+ *  Created on: 3 may. 2018
+ *      Author: utnso
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <commons/collections/list.h>
+
+typedef struct {
+
+	//info gral
+	int cant_entradas;
+	int tam_total;
+
+	//para lsu
+	int cant_entradas_vacias;
+
+	//para ke
+	char primer_letra;
+	char ult_letra;
+
+
+} instancia;
+
+t_list instancias_disponibles;
+
+#endif /* MAIN_H_ */
