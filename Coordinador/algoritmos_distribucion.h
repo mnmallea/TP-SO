f0d53f954915f623f2a3d098bba8ab26b4d341f9
Nayla Winter <naylawinter@gmail.com>
2018-05-03 22:17:02 -0300

    Primer boceto instancias/lista instancias

--- Coordinador/algoritmos_distribucion.h
+++ Coordinador/algoritmos_distribucion.h
@@ -7,8 +7,13 @@
 
 #ifndef ALGORITMOS_DISTRIBUCION_H_
 #define ALGORITMOS_DISTRIBUCION_H_
+#include <commons/collections/list.h>
 
 typedef enum {LSU, EL, KE} tipo_algoritmo_dist;
+
+void obtener_instancia_segun_EL(t_list* instancias);
+void obtener_instancia_segun_LSU(t_list* instancias);
+void obtener_instancia_segun_KE(t_list* instancias);
 
 
 #endif /* ALGORITMOS_DISTRIBUCION_H_ */
