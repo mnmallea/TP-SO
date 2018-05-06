5be9e650322e657964f3e4538011d43b952d5fa2
Nayla Winter <naylawinter@gmail.com>
2018-05-05 22:27:27 -0300

    Obtener instancia por EL

--- Coordinador/algoritmos_distribucion.h
+++ Coordinador/algoritmos_distribucion.h
@@ -8,12 +8,14 @@
 #ifndef ALGORITMOS_DISTRIBUCION_H_
 #define ALGORITMOS_DISTRIBUCION_H_
 #include <commons/collections/list.h>
+#include "main.h"
+
 
 typedef enum {LSU, EL, KE} tipo_algoritmo_dist;
 
-void obtener_instancia_segun_EL(t_list* instancias);
-void obtener_instancia_segun_LSU(t_list* instancias);
-void obtener_instancia_segun_KE(t_list* instancias);
+t_instancia obtener_instancia_segun_EL(t_list* instancias);
+t_instancia obtener_instancia_segun_LSU(t_list* instancias);
+t_instancia obtener_instancia_segun_KE(t_list* instancias);
 
 
 #endif /* ALGORITMOS_DISTRIBUCION_H_ */
