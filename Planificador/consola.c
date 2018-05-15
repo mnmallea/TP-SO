9d28d7341d549b6ce844c635de469246af429be4
Paniaton <lpanizza@est.frba.utn.edu.ar>
2018-05-14 21:16:39 -0300

    mini cambios consola

--- Planificador/consola.c
+++ Planificador/consola.c
@@ -17,26 +17,37 @@ void *menu(void *ptr){
 	char *clave = (char*)malloc(40);
 	int recurso;
 	int id;
+	int flag=0;
 
-		printf("Ingrese la opcion que desea ejecutar: "
-					"\n 1: para pausar/continuar la ejecucion del planificador "
-					"\n 2 <clave> <ID>: para bloquear un proceso"
-					"\n 3 <clave>: para desbloquear un proceso bloqueado "
-					"\n 4 <recurso>: listar procesos esperando al recurso"
-					"\n 5 <ID>: para matar un proceso"
-					"\n 6 <clave>: para conocer el status de la clave"
-					"\n 7: para solucionar conflictos de deadlock "
-					"\n 0: para salir de la consola \n"
-					);
+		do{
 
-		scanf("%d", &opcion_seleccionada);
+			printf("Ingrese la opcion que desea ejecutar: "
+								"\n 1: para pausar/continuar la ejecucion del planificador "
+								"\n 2 <clave> <ID>: para bloquear un proceso"
+								"\n 3 <clave>: para desbloquear un proceso bloqueado "
+								"\n 4 <recurso>: listar procesos esperando al recurso"
+								"\n 5 <ID>: para matar un proceso"
+								"\n 6 <clave>: para conocer el status de la clave"
+								"\n 7: para solucionar conflictos de deadlock "
+								"\n 0: para salir de la consola \n"
+								);
 
-		while( opcion_seleccionada != 0 ){
+			scanf("%d", &opcion_seleccionada);
 
 			switch(opcion_seleccionada){
 			case 1:
-				printf("Ingreso pausar/despausar la consola");
-				break;
+				if(flag==0){
+					printf("Planificador pausado\n");
+					flag++;
+					break;
+				}
+				else{
+					printf("Continuando con la planificacion\n");
+					flag--;
+					break;
+				}
+
+
 			case 2:
 				printf("Ingreso bloquear un proceso, ingrese <clave>");
 
@@ -75,11 +86,11 @@ void *menu(void *ptr){
 				break;
 			default:
 				break;
+
 			}
 
-			scanf("%d", &opcion_seleccionada);
 
-		}
+		}while(opcion_seleccionada != 0);
 
 	printf("Ha salido de la consola");
 	free(clave);
