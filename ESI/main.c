b760bd8ead74cf98c8d53120e375ee226e1eea82
Paniaton <lpanizza@est.frba.utn.edu.ar>
2018-06-03 18:43:28 -0300

    cambios esi

--- ESI/main.c
+++ ESI/main.c
@@ -34,11 +34,12 @@ int main(int argc, char* argv[]){
 	safe_send(socketCord, esi_id, sizeof(int));
 
 
-	//recibir_confirmacion(socketPlan); //signal para ejecutar
+	recibir_confirmacion(socketPlan); //signal para ejecutar
 
 	while ((read = getline(&line, &len, fp)) != -1)
 	{
-
+		size_t s_valor = 0;
+		size_t s_clave = 0;
 		t_esi_operacion parsed = parse(line);
 
 		if((n_esi_operacion=malloc(sizeof(t_operacion)))==NULL){
@@ -53,42 +54,40 @@ int main(int argc, char* argv[]){
 	    	log_debug(logger, "Crea pkg");
 	    	switch(parsed.keyword){
 	    	 	case GET:
+	    	 		log_debug(logger, "Get");
 	    	 		n_esi_operacion->keyword=0;
 
-	    	 		len=strlen(parsed.argumentos.GET.clave)+1;
-	    	       	n_esi_operacion->clave=pedirEspacio(sizeof(char)*len);
+	    	 		s_clave=sizeof(char)*strlen(parsed.argumentos.GET.clave)+1;
+	    	       	n_esi_operacion->clave=pedirEspacio(s_clave);
 	    	       	strcpy(n_esi_operacion->clave,parsed.argumentos.GET.clave);
 
-	    	    	agregar(pkg_esi,n_esi_operacion,sizeof(n_operacion));
-	        		eviarPaquete(socketCord,pkg_esi);
+	    	       	cargarPkg(pkg_esi,n_esi_operacion,s_clave+sizeof(int));
 
-
-	    	    	agregar(pkg_esi,n_esi_operacion,sizeof(n_operacion));
-	        		eviarPaquete(socketCord,pkg_esi);
 	        		free(n_esi_operacion->clave);
-	        		free(n_esi_operacion);
 
 	        		break;
 	            case SET:
+
+	      	 		log_debug(logger, "Set");
 	            	if(strlen(parsed.argumentos.SET.valor)<40){
+
 	            		n_esi_operacion->keyword=1;
 
+	            		s_clave=sizeof(char)*strlen(parsed.argumentos.SET.clave)+1;
+	            		n_esi_operacion->clave=pedirEspacio(s_clave);
 
-	            		len=strlen(parsed.argumentos.SET.clave)+1;
-	            		n_esi_operacion->clave=pedirEspacio(sizeof(char)*len);
-
-	            		len=strlen(parsed.argumentos.SET.valor)+1;
-	            		n_esi_operacion->valor=pedirEspacio(sizeof(char)*len);
+	            		s_valor=sizeof(char)*strlen(parsed.argumentos.SET.valor)+1;
+	            		n_esi_operacion->valor=pedirEspacio(s_valor);
 
 	            		strcpy(n_esi_operacion->clave,parsed.argumentos.SET.clave);
 	            		strcpy(n_esi_operacion->valor,parsed.argumentos.SET.valor);
 
+	            		//log_debug(logger, "Size de oper_esi: %d", sizeof(*n_esi_operacion));
 
-		    	    	agregar(pkg_esi,n_esi_operacion,sizeof(n_operacion));
-		        		eviarPaquete(socketCord,pkg_esi);
+	            		cargarPkg(pkg_esi,n_esi_operacion,s_clave+s_valor+sizeof(int));
+
 		        		free(n_esi_operacion->clave);
 		        		free(n_esi_operacion->valor);
-		        		free(n_esi_operacion);
 
 	            	}
 	            	else{
@@ -99,16 +98,16 @@ int main(int argc, char* argv[]){
 
 	            	break;
 	            case STORE:
+	      	 		log_debug(logger, "Store");
 	            	n_esi_operacion->keyword=2;
 
-	            	len=strlen(parsed.argumentos.STORE.clave)+1;
-	            	n_esi_operacion->clave=pedirEspacio(sizeof(char)*len);
+	            	s_clave=sizeof(char)*strlen(parsed.argumentos.STORE.clave)+1;
+	            	n_esi_operacion->clave=pedirEspacio(s_clave);
 	            	strcpy(n_esi_operacion->clave,parsed.argumentos.STORE.clave);
 
-	    	    	agregar(pkg_esi,n_esi_operacion,sizeof(n_operacion));
-	        		eviarPaquete(socketCord,pkg_esi);
+            		cargarPkg(pkg_esi,n_esi_operacion,s_clave+sizeof(int));
+
 	        		free(n_esi_operacion->clave);
-	        		free(n_esi_operacion);
 
 	            	break;
 	            default:
@@ -117,9 +116,7 @@ int main(int argc, char* argv[]){
 	            	exit_gracefully(1);
 	    	}
 
-	    	agregar(pkg_esi,n_esi_operacion,sizeof(n_operacion));
-
-    		eviarPaquete(socketCord,pkg_esi);
+	    	enviarPkg(socketCord,pkg_esi);
 
 /*
 	    	if(recibir_mensaje(socketCord)==1)
@@ -127,6 +124,7 @@ int main(int argc, char* argv[]){
 	    	else
 	    		mandar_error(socketPlan);
 */
+
 	    	destruirPaquete(pkg_esi);
 	    	destruir_operacion(parsed);
 	    }
@@ -136,9 +134,9 @@ int main(int argc, char* argv[]){
 	    	//enviar respuesta al planificador, error de linea(?)
 	    	exit_gracefully(1);
 	    }
-	   // recibir_confirmacion(socketPlan);
+	   recibir_confirmacion(socketPlan);
 	}
-  
+
 /*
   destruir_operacion(parsed);
 	        	//hace falta el wait?
