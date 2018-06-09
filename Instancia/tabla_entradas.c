b3719e4eff953133994a57577a3e7b84f9d83876
mnmallea <mnmallea@gmail.com>
2018-06-09 00:43:29 -0300

    bug fixes menores en instancia. (ch de menores)

--- Instancia/tabla_entradas.c
+++ Instancia/tabla_entradas.c
@@ -5,82 +5,88 @@ void crearTablaEntradas() {
 	entradasLibres = obtenerEntradasTotales();
 }
 
-
-bool agregarEnTabla (int nuevaEntrada, claveEntrada* claveE){
+bool agregarEnTabla(int nuevaEntrada, claveEntrada* claveE) {
 	tablaE * nuevaE = adaptoClave(claveE);
-	nuevaE -> numero = nuevaEntrada;
-	if(nuevaEntrada >= 0){
-		list_add(tabla,(void *)nuevaE);
-		list_sort(tabla,ordenAscendente);
-		entradasLibres -= claveE->tamanio/obtenerTamanioEntrada()+1;
+	nuevaE->numero = nuevaEntrada;
+	if (nuevaEntrada >= 0) {
+		list_add(tabla, (void *) nuevaE);
+		list_sort(tabla, ordenAscendente);
+		entradasLibres -= claveE->tamanio / obtenerTamanioEntrada() + 1;
 		return true;
 	}
 	return false;
 }
 
-bool ordenAscendente(void * primero, void * segundo){
+bool ordenAscendente(void * primero, void * segundo) {
 	tablaE* unaEntrada = (tablaE*) primero;
 	tablaE* otraEntrada = (tablaE*) segundo;
-	if (otraEntrada->numero < unaEntrada->numero){
+	if (otraEntrada->numero < unaEntrada->numero) {
 		return false;
 	}
 	return true;
 }
 
-bool tablaEstaVacia(t_list* tabla){
-	if (tabla == NULL){
-		return true;
-	}
-	return false;
+bool tablaEstaVacia(t_list* tabla) {
+	return list_is_empty(tabla);
 }
 
-int entradaSiguienteEnTabla(claveEntrada* claveE){
+int entradaSiguienteEnTabla(claveEntrada* claveE) {
 	tablaE* entrada1;
 	tablaE* entrada2;
 	// me fijo la cantidad de entradas usadas
 	int cantEntradasUsadas = list_size(tabla);
 	// me fijo cuantas entradas necesitria la clave que llega por parametro
-	int cantEntradasNecesarias = claveE->tamanio/obtenerTamanioEntrada()+1;
+	int cantEntradasNecesarias = claveE->tamanio / obtenerTamanioEntrada() + 1;
 	//defino una variable para chequear el espacio entre entradas de la tabla
 	int espacioEntreEntradas = 0;
-	// si la tabla no esta vacia y las entradas libres me alcanzan para cubrir las que necesito empiezo a ejecutar
-	if(entradasLibres >=cantEntradasNecesarias && !tablaEstaVacia(tabla)){
-			//if(cantEntradasUsadas == 0){
-			//	return 0;
-		//	} else{
-				entrada1 = (tablaE*) list_get(tabla,0);
-				if (entrada1->numero > 0 && cantEntradasNecesarias <= (entrada1->numero)){
-						return 0;
-					}
 
-
-			int a = 0;
-			//va a ir hasta el anteultimo de la lista
-			while (cantEntradasUsadas > a+1){
-				//tengo una entrada y su siguiente
-				entrada1 = (tablaE*) list_get(tabla,a);
-				entrada2 = (tablaE*) list_get(tabla,a+1);
-				// chequeo el espacio entre la primera y la segunda entrada
-				espacioEntreEntradas = (entrada2->numero) - (entrada1->numero + (entrada1->tamanio/obtenerTamanioEntrada()+1));
-				//si la cantidad de entradas necesarias para meter la que llego por parametro es menor al espacio entre entradas
-				if(cantEntradasNecesarias <= espacioEntreEntradas){
-					//retorno la posicion donde podria insertar esa clave
-					return a + (entrada1->tamanio/obtenerTamanioEntrada()+1);
-				}
-			a++;
-			}
-			entrada2 = (tablaE*) list_get(tabla,list_size(tabla)-1);
-			if(obtenerTamanioEntrada()- entrada2->numero>= cantEntradasNecesarias){
-				return (entrada2->tamanio/obtenerTamanioEntrada()+ 1) + entrada2->numero;
-				}
-
-		}
-//arreglar tiene que decir que hubo error y exit_gracefully o algo asi
-return -1;
+	if(tablaEstaVacia(tabla)){//si es la primera vez todo revisar esto
+		return 0;
 	}
 
+	// si la tabla no esta vacia y las entradas libres me alcanzan para cubrir las que necesito empiezo a ejecutar
+	if (entradasLibres >= cantEntradasNecesarias) {
+		//if(cantEntradasUsadas == 0){
+		//	return 0;
+		//	} else{
+		entrada1 = (tablaE*) list_get(tabla, 0);
+		if (entrada1->numero > 0
+				&& cantEntradasNecesarias <= (entrada1->numero)) {
+			return 0;
+		}
 
-tablaE * adaptoClave(claveEntrada * claveE){
+		int a = 0;
+		//va a ir hasta el anteultimo de la lista
+		while (cantEntradasUsadas > a + 1) {
+			//tengo una entrada y su siguiente
+			entrada1 = (tablaE*) list_get(tabla, a);
+			entrada2 = (tablaE*) list_get(tabla, a + 1);
+			// chequeo el espacio entre la primera y la segunda entrada
+			espacioEntreEntradas =
+					(entrada2->numero)
+							- (entrada1->numero
+									+ (entrada1->tamanio
+											/ obtenerTamanioEntrada() + 1));
+			//si la cantidad de entradas necesarias para meter la que llego por parametro es menor al espacio entre entradas
+			if (cantEntradasNecesarias <= espacioEntreEntradas) {
+				//retorno la posicion donde podria insertar esa clave
+				return a + (entrada1->tamanio / obtenerTamanioEntrada() + 1);
+			}
+			a++;
+		}
+		entrada2 = (tablaE*) list_get(tabla, list_size(tabla) - 1);
+		if (obtenerTamanioEntrada() - entrada2->numero
+				>= cantEntradasNecesarias) {
+			return (entrada2->tamanio / obtenerTamanioEntrada() + 1)
+					+ entrada2->numero;
+		}
+
+	}
+//arreglar tiene que decir que hubo error y exit_gracefully o algo asi
+	return -1;
+}
+
+tablaE * adaptoClave(claveEntrada * claveE) {
 	tablaE * entrada = malloc(sizeof(tablaE));
 //Si strdup no encuentra espacio para el nuevo string, entonces devuelve un puntero null
 	entrada->clave = strdup(claveE->clave);
@@ -88,29 +94,29 @@ tablaE * adaptoClave(claveEntrada * claveE){
 	return entrada;
 }
 
-bool quitarDeTabla(claveEntrada * claveE){
-	int i=0;
-	while (i < list_size(tabla)){
-		tablaE* entrada = (tablaE*) list_get(tabla,i);
-		if (!strcmp(claveE->clave,entrada->clave)){
-			removerDeLista(i,entrada);
+bool quitarDeTabla(claveEntrada * claveE) {
+	int i = 0;
+	while (i < list_size(tabla)) {
+		tablaE* entrada = (tablaE*) list_get(tabla, i);
+		if (!strcmp(claveE->clave, entrada->clave)) {
+			removerDeLista(i, entrada);
 			return true;
 		}
-	i++;
+		i++;
 	}
 	return false;
 }
 
-void removerDeLista(int unaVariable, tablaE* entrada ){
-	list_remove(tabla,unaVariable);
-	entradasLibres += (entrada->tamanio/obtenerTamanioEntrada())+1;
+void removerDeLista(int unaVariable, tablaE* entrada) {
+	list_remove(tabla, unaVariable);
+	entradasLibres += (entrada->tamanio / obtenerTamanioEntrada()) + 1;
 }
 
-void mostrarTabla(){
-	int i=0;
-	while (list_size(tabla) > i){
-		tablaE* entrada = (tablaE*) list_get(tabla,i);
-		printf("----Registro n° %d----\n",i);
+void mostrarTabla() {
+	int i = 0;
+	while (list_size(tabla) > i) {
+		tablaE* entrada = (tablaE*) list_get(tabla, i);
+		printf("----Registro n° %d----\n", i);
 		printf("----Posicion Almacenamiento: %d", entrada->numero);
 		printf("----La Clave es: %s", entrada->clave);
 		printf("----El Tamanio es: %d \n", entrada->tamanio);
@@ -119,11 +125,11 @@ void mostrarTabla(){
 	}
 }
 
-void liberarEntrada(tablaE* entrada){
+void liberarEntrada(tablaE* entrada) {
 	free(entrada->clave);
 	free(entrada);
 }
-void liberarCv(claveEntrada* cv){
+void liberarCv(claveEntrada* cv) {
 	free(cv->clave);
 	free(cv->valor);
 	free(cv);
@@ -137,20 +143,19 @@ claveEntrada* crearClaveEntrada(char* clave, char* valor) {
 	return claveE;
 }
 
-
-bool hayEntradasDisponibles(claveEntrada* cv){
-	return (cv->tamanio/obtenerTamanioEntrada() +1)<= entradasLibres;
+bool hayEntradasDisponibles(claveEntrada* cv) {
+	return (cv->tamanio / obtenerTamanioEntrada() + 1) <= entradasLibres;
 
 }
 
-tablaE* buscarEntrada(char* claveAPedir){
+tablaE* buscarEntrada(char* claveAPedir) {
 	tablaE* entrada;
-	for(int i=0;i<list_size(tabla);i++){
-		entrada = (tablaE*) list_get(tabla,i);
-		if(entrada->clave == claveAPedir){
+	for (int i = 0; i < list_size(tabla); i++) {
+		entrada = (tablaE*) list_get(tabla, i);
+		if (!strcmp(claveAPedir, entrada->clave)) {
 			return entrada;
 		}
 	}
-	log_info(logger,"no se encontro la entrada buscada");
+	log_info(logger, "no se encontro la entrada buscada");
 	return NULL;
 }
