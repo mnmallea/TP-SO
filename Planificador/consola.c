a36617ed6420ee874295b9165b55610465f5f48c
Nayla Winter <naylawinter@gmail.com>
2018-07-03 23:39:27 -0300

    Semaforo para el esi corriendo

--- Planificador/consola.c
+++ Planificador/consola.c
@@ -24,8 +24,7 @@ void *menu(void *ptr) {
 				"\n 6 <clave>: para conocer el status de la clave"
 				"\n 7: para solucionar conflictos de deadlock "
 				"\n 0: para salir de la consola \n"
-				"\nOpcion n°:"
-				);
+				"\nOpcion n°:");
 
 		scanf("%d", &opcion_seleccionada);
 
@@ -92,8 +91,6 @@ void *menu(void *ptr) {
 
 		system("clear");
 
-
-
 	} while (opcion_seleccionada != 0);
 
 	printf("Ha salido de la consola");
@@ -107,12 +104,14 @@ void pausar_despausar_consola() {
 	pthread_mutex_lock(&mutex_pausa);
 	if (planificacion_pausada) {
 		printf("Continuando con la planificacion\n");
-		log_debug(logger, "CONSOLA: pausar/despausar. Actualmente consola pausada. Se continua planificacion");
+		log_debug(logger,
+				"CONSOLA: pausar/despausar. Actualmente consola pausada. Se continua planificacion");
 		planificacion_pausada = false;
 		sem_post(&pausa_planificacion);
 	} else {
 		printf("Planificador pausado\n");
-		log_debug(logger, "CONSOLA: pausar/despausar. Actualmente consola planificando. Se pausa la planificacion");
+		log_debug(logger,
+				"CONSOLA: pausar/despausar. Actualmente consola planificando. Se pausa la planificacion");
 		planificacion_pausada = true;
 	}
 	pthread_mutex_unlock(&mutex_pausa);
@@ -126,19 +125,23 @@ void listar(char* rec) {
 
 		pthread_mutex_unlock(&mutex_dic_esis_bloqueados);
 		printf("La clave solicitada(%s) no tiene ningun ESI bloqueado", rec);
-		log_debug(logger, "CONSOLA: Se eligio listar los esis bloqueados para la clave: %s", rec);
+		log_debug(logger,
+				"CONSOLA: Se eligio listar los esis bloqueados para la clave: %s",
+				rec);
 
 	} else {
 
-		printf("Se procede a listar los ESIS encolados para la clave: %s\n", rec);
-		log_debug(logger, "CONSOLA: Se listaran los esis encolados para la clave: %s", rec);
+		printf("Se procede a listar los ESIS encolados para la clave: %s\n",
+				rec);
+		log_debug(logger,
+				"CONSOLA: Se listaran los esis encolados para la clave: %s",
+				rec);
 
 		t_list* esis_bloq = dictionary_get(dic_esis_bloqueados, rec);
 		pthread_mutex_unlock(&mutex_dic_esis_bloqueados);
 		list_iterate(esis_bloq, mostrar_esi_en_pantalla);
 
 	}
-
 
 }
 
@@ -150,21 +153,34 @@ void mostrar_esi_en_pantalla(void* esi) {
 void bloquear(char* clave, int id) {
 
 	printf("Se procede a bloquear al esi: %d para la clave: %s", id, clave);
-	log_debug(logger, "CONSOLA: Se eligio bloquear al esi: %d para la clave %s", id, clave);
+	log_debug(logger, "CONSOLA: Se eligio bloquear al esi: %d para la clave %s",
+			id, clave);
 
-	if(esi_corriendo->id == id){
+	pthread_mutex_lock(&mutex_esi_corriendo);
+	if (esi_corriendo->id == id) {
+		pthread_mutex_unlock(&mutex_esi_corriendo);
 		//es el esi corriendo
 		//TODO: ver q hacemos aca
-	}else if(es_un_esi_listo(id)){
-		t_esi* esi_a_bloquear = obtener_de_listos(id);
-		eliminar_de_listos(esi_a_bloquear);
-		bloquear_esi(clave, esi_a_bloquear);
-	}else if(es_un_esi_bloqueado(id)){
-		printf("El esi solicitado para el bloqueo(%d) ya se encontraba bloqueado", id);
-	}else if(es_un_esi_finalizado(id)){
-		printf("El esi solicitado para el bloqueo(%d) ya se encontraba finalizado", id);
-	}else{
-		printf("El esi solicitado para el bloqueo(%d) no existe en el sistema", id);
+	} else {
+		pthread_mutex_unlock(&mutex_esi_corriendo);
+		if (es_un_esi_listo(id)) {
+
+			t_esi* esi_a_bloquear = obtener_de_listos(id);
+			eliminar_de_listos(esi_a_bloquear);
+			bloquear_esi(clave, esi_a_bloquear);
+		} else if (es_un_esi_bloqueado(id)) {
+			printf(
+					"El esi solicitado para el bloqueo(%d) ya se encontraba bloqueado",
+					id);
+		} else if (es_un_esi_finalizado(id)) {
+			printf(
+					"El esi solicitado para el bloqueo(%d) ya se encontraba finalizado",
+					id);
+		} else {
+			printf(
+					"El esi solicitado para el bloqueo(%d) no existe en el sistema",
+					id);
+		}
 	}
 }
 
@@ -173,23 +189,31 @@ void matar_por_consola(int id) {
 	printf("Se procede a matar al esi: %d", id);
 	log_debug(logger, "CONSOLA: Se eligio matar al esi: %d", id);
 
-		if(esi_corriendo->id == id){
-			//es el esi corriendo
-			//TODO: ver q hacemos aca
-		}else if(es_un_esi_listo(id)){
+	pthread_mutex_lock(&mutex_esi_corriendo);
+	if (esi_corriendo->id == id) {
+		pthread_mutex_unlock(&mutex_esi_corriendo);
+		//es el esi corriendo
+		//TODO: ver q hacemos aca
+	} else {
+		pthread_mutex_unlock(&mutex_esi_corriendo);
+		if (es_un_esi_listo(id)) {
+
 			t_esi* esi_a_matar = obtener_de_listos(id);
 			eliminar_de_listos(esi_a_matar);
 			finalizar_esi(esi_a_matar);
-		}else if(es_un_esi_finalizado(id)){
-			printf("El esi solicitado para matar(%d) ya se encontraba finalizado", id);
-		}else if(es_un_esi_bloqueado(id)){
+		} else if (es_un_esi_finalizado(id)) {
+			printf(
+					"El esi solicitado para matar(%d) ya se encontraba finalizado",
+					id);
+		} else if (es_un_esi_bloqueado(id)) {
 			t_esi* esi_a_matar = obtener_de_bloqueados(id);
 			eliminar_de_bloqueados(esi_a_matar);
 			finalizar_esi(esi_a_matar);
-		}else{
-			printf("El esi solicitado para matar(%d) no existe en el sistema", id);
+		} else {
+			printf("El esi solicitado para matar(%d) no existe en el sistema",
+					id);
 		}
-
+	}
 }
 
 void envia_status_clave(char* clave) {
@@ -201,17 +225,18 @@ void envia_status_clave(char* clave) {
 	paquete_agregar(paquete, clave, strlen(clave) + 1);
 	if (paquete_enviar_con_codigo(paquete, SOLICITUD_STATUS_CLAVE, socketCord)
 			< 0) {
-		log_error(logger, "Error enviandole el paquete de status clave al coordindor");
+		log_error(logger,
+				"Error enviandole el paquete de status clave al coordindor");
 		paquete_destruir(paquete);
 		exit(EXIT_FAILURE);
-	}else{
+	} else {
 		while (1) {
 			sem_wait(&coordinador_respondio_paq);
 
-				//todo:  acceder a la variable compartida
-				//ponerle mutex a la variable
-				//hacer printf de las cosas que me mando
-			}
+			//todo:  acceder a la variable compartida
+			//ponerle mutex a la variable
+			//hacer printf de las cosas que me mando
+		}
 	}
 	paquete_destruir(paquete);
 
