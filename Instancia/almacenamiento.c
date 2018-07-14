e5e366ae42f8f2e4ef0f1727ee6c1c80c118e57a
Nayla Winter <naylawinter@gmail.com>
2018-07-14 13:06:49 -0300

    Agrego funciones para bitarray

--- Instancia/almacenamiento.c
+++ Instancia/almacenamiento.c
@@ -9,16 +9,23 @@ void inicializarAlmacenamiento(unsigned int entradas,
 		unsigned int tamanioEntrada) {
 	log_info(logger, "inicio el almacenamiento");
 	ato = malloc(sizeof(almacenamiento));
-	estadoAto= crearBitArray(entradas);
 	ato->cantEntradas = entradas;
 	ato->tamanioEntrada = tamanioEntrada;
 	ato->dato = calloc(ato->cantEntradas, ato->tamanioEntrada);
 	log_info(logger, "inicio el almacenamiento");
+	bitarray_almac = crearBitArray(ato->cantEntradas);
+	log_debug(logger, "Se ha creado un bitarray de tamaño %d",
+			bitarray_get_max_bit(bitarray_almac));
+	log_debug(logger, "El espacio restante (total) del almacenamiento es %d",
+			almac_espacio_disponible());
+	log_debug(logger, "La primera entrada libre es %d", almac_primera_posicion_libre_con_tamanio(2));
 }
 
 void setEnAlmacenamiento(int proximaEntrada, void* valor, unsigned int tamanio) {
 
-	log_trace(logger, "ALMACENAMIENTO: se procede a guardar el valor %s en la entrada %d", (char*)valor, proximaEntrada);
+	log_trace(logger,
+			"ALMACENAMIENTO: se procede a guardar el valor %s en la entrada %d",
+			(char*) valor, proximaEntrada);
 
 	if (tamanio <= ato->tamanioEntrada) {
 		log_trace(logger, "El valor a guardar es atomico");
@@ -26,7 +33,8 @@ void setEnAlmacenamiento(int proximaEntrada, void* valor, unsigned int tamanio) 
 				tamanio);
 
 	} else {
-		log_trace(logger, "El valor a guardar no es atomico, ocupara varias entradas");
+		log_trace(logger,
+				"El valor a guardar no es atomico, ocupara varias entradas");
 		unsigned int resto = tamanio - ato->tamanioEntrada;
 		void* parteAinsertar = malloc(ato->tamanioEntrada);
 		void* parteRestante = malloc(resto);
@@ -46,25 +54,25 @@ void setEnAlmacenamiento(int proximaEntrada, void* valor, unsigned int tamanio) 
 }
 
 /*
-char* convertirString(const void *valor, size_t tamanio) {
-	char *valorStr = calloc(tamanio + 1, sizeof(tamanio));
-	memcpy(valorStr, valor, tamanio);
+ char* convertirString(const void *valor, size_t tamanio) {
+ char *valorStr = calloc(tamanio + 1, sizeof(tamanio));
+ memcpy(valorStr, valor, tamanio);
 
-	return valorStr;
-}
-void mostrarAlmacenamiento(){
-	int a=0;
-	while(a < ato->cantEntradas){
-		void* valor= malloc(ato->tamanioEntrada);
-		memcpy(valor,ato->dato + (a *ato->tamanioEntrada),ato->tamanioEntrada);;
-		char* valorStr= convertirString(valor,ato->tamanioEntrada);
-		free(valor);
-		printf("imprimiendo la entrada numero %d : %s",a,valorStr);
-		free(valorStr);
-		a++;
-	}
-}
-*/
+ return valorStr;
+ }
+ void mostrarAlmacenamiento(){
+ int a=0;
+ while(a < ato->cantEntradas){
+ void* valor= malloc(ato->tamanioEntrada);
+ memcpy(valor,ato->dato + (a *ato->tamanioEntrada),ato->tamanioEntrada);;
+ char* valorStr= convertirString(valor,ato->tamanioEntrada);
+ free(valor);
+ printf("imprimiendo la entrada numero %d : %s",a,valorStr);
+ free(valorStr);
+ a++;
+ }
+ }
+ */
 
 void eliminarAlmacenamiento() {
 	free(ato->dato);
@@ -72,24 +80,90 @@ void eliminarAlmacenamiento() {
 	log_info(logger, "memoria liberada");
 }
 
-void* buscarEnALmacenamiento(int posicion, unsigned int tamanio){
-	void*carga =malloc(tamanio);
-	memcpy(carga,ato->dato + (posicion * ato->tamanioEntrada),tamanio);
+void* buscarEnALmacenamiento(int posicion, unsigned int tamanio) {
+	void*carga = malloc(tamanio);
+	memcpy(carga, ato->dato + (posicion * ato->tamanioEntrada), tamanio);
 	return carga;
 }
 
-
-
-t_bitarray * crearBitArray(uint32_t cantBloques){
-int tamanioBitarray=cantBloques/8;
-if(cantBloques % 8 != 0){
-tamanioBitarray++;
+t_bitarray * crearBitArray(int cantBloques) {
+	int tamanioBitarray = cantBloques / 8;
+	if (cantBloques % 8 != 0) {
+		tamanioBitarray++;
+	}
+	char* bits = malloc(tamanioBitarray);
+	t_bitarray * bitarray = bitarray_create_with_mode(bits, tamanioBitarray,
+			MSB_FIRST);
+	int cont = 0;
+	for (; cont < tamanioBitarray * 8; cont++) {
+		bitarray_clean_bit(bitarray, cont);
+	}
+	return bitarray;
 }
-char* bits=malloc(tamanioBitarray);
-t_bitarray * bitarray = bitarray_create_with_mode(bits,tamanioBitarray,MSB_FIRST);
-int cont=0;
-for(; cont < tamanioBitarray*8; cont++){
-bitarray_clean_bit(bitarray, cont);
+
+/*
+ * Devuelve cual es la primera posicion libre desde el index que le das
+ * Si no quedan posiciones libre devuelve -1
+ */
+int almac_primera_posicion_libre_desde(int index_inicio) {
+	int i;
+	for (i = index_inicio; i < ato->cantEntradas; i++) {
+		if (!bitarray_test_bit(bitarray_almac, i)) {
+			return i;
+		}
+	}
+	return -1;
 }
-return bitarray;
+
+int almac_cant_entradas_libres_desde(int index) {
+	int i;
+	int contador = 0;
+	for (i = index; i < ato->cantEntradas; i++) {
+		if (bitarray_test_bit(bitarray_almac, i)) {
+			return contador;
+		}
+		contador ++;
+	}
+	return contador;
+}
+
+int almac_primera_posicion_libre_con_tamanio(int size) {
+	for (int i = 0; i < ato->cantEntradas; i++) {
+		if (almac_cant_entradas_libres_desde(i) >= size) {
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+/*
+ * Setea como ocupadas las entradas en el bitarray
+ * Devuelve -1 si falla
+ */
+int almac_ocupar_entradas(int index_inicio, int cantidad_entradas) {
+	int index_final = index_inicio + cantidad_entradas - 1;
+	if (index_final > ato->cantEntradas) {
+		log_warning(logger,
+				"Se está intentando ocupar mas entradas que las que tiene el almacenamiento");
+		return -1;
+	}
+	int i;
+	for (i = 0; i <= index_final; i++) {
+		bitarray_set_bit(bitarray_almac, i);
+	}
+	return 0;
+}
+
+/*
+ * Devuelve el espacio restante en el almacenamiento
+ */
+int almac_espacio_disponible() {
+	int i;
+	int contador_espacio = 0;
+	for (i = 0; i < ato->cantEntradas; i++) {
+		if (!bitarray_test_bit(bitarray_almac, i))
+			contador_espacio++;
+	}
+	return contador_espacio;
 }
--- Instancia/almacenamiento.h
+++ Instancia/almacenamiento.h
@@ -19,16 +19,21 @@ typedef struct {
 
 almacenamiento* ato;
 
-t_bitarray* estadoAto;
+t_bitarray* bitarray_almac;
 
 void almacenamiento_logger();
 void mostrarAlmacenamiento();
 void eliminarAlmacenamiento();
-t_bitarray * crearBitArray(uint32_t cantBloques);
+t_bitarray * crearBitArray(int cantBloques);
 char* convertirString(const void *valor, size_t tamanio);
 void* buscarEnALmacenamiento(int posicion, unsigned int tamanio);
 void inicializarAlmacenamiento(unsigned int entradas,unsigned int tamanioEntrada);
 void setEnAlmacenamiento(int proximaEntrada, void* valor, unsigned int tamanio);
+
+int almac_primera_posicion_libre_desde(int index_inicio);
+int almac_espacio_disponible();
+int almac_cant_entradas_libres_desde(int index);
+int almac_primera_posicion_libre_con_tamanio(int size);
 
 
 #endif /* ALMACENAMIENTO_H_ */
