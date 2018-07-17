a3a1c97a597c0954c53d208bf67eaa4754595974
Nayla Winter <naylawinter@gmail.com>
2018-07-17 18:25:07 -0300

    Sjf con desalojo tiene remaining time

--- Planificador/algoritmos_planificacion.h
+++ Planificador/algoritmos_planificacion.h
@@ -20,15 +20,18 @@
 #include "typedefs.h"
 
 extern config configuracion;
+extern t_esi* esi_corriendo;
 
 t_esi *obtener_proximo_segun_fifo(t_list *lista_esis);
 t_esi *obtener_proximo_segun_sjf(t_list *lista_esis);
+t_esi *obtener_proximo_segun_sjfcd(t_list *lista_esis);
 t_esi *obtener_proximo_segun_hrrn(t_list *lista_esis);
 t_esi* remover_esi_de_lista(t_list*, int);
 
 void obtener_proximas_rafagas(void* esi);
 void obtener_rr(void* esi);
 
+double remaining_time_esi_corriendo();
 bool menor_estimacion(void* esi1, void *esi2);
 bool mayor_response_ratio(void* esi1, void* esi2);
 
