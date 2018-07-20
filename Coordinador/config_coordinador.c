aabc52c731880bc3aaa7125a0c11a15b1ec8c4e3
mnmallea <mnmallea@gmail.com>
2018-07-19 21:02:52 -0300

    Arreglo cantidad parametros coordinador

--- Coordinador/config_coordinador.c
+++ Coordinador/config_coordinador.c
@@ -12,46 +12,58 @@
 #include "config_coordinador.h"
 #include "error.h"
 
-void configurar(char *ruta){
-	log_info(logger,"Configurando coordinador");
+void configurar(char *ruta) {
+	log_info(logger, "Configurando coordinador");
 
 	t_config *config_dictionary = config_create(ruta);
 
-	if(config_dictionary == NULL){
-		exit_error_with_msg("No se pudo crear la configuración (no se encuentra el archivo)");
+	if (config_dictionary == NULL) {
+		log_error(logger,
+				"No se pudo crear la configuración (no se encuentra el archivo)");
+		log_destroy(logger);
+		exit(EXIT_FAILURE);
 	}
 
-	if(		config_has_property(config_dictionary, "PORT") &&
-			config_has_property(config_dictionary, "ALGORITMO") &&
-			config_has_property(config_dictionary, "CANT_ENTRADAS") &&
-			config_has_property(config_dictionary, "ENTRADA_SIZE") &&
-			config_has_property(config_dictionary, "RETARDO")){
-		strncpy(configuracion.puerto, config_get_string_value(config_dictionary, "PORT"),PORT_MAX_STRING_LENGTH);//hay que copiarselo porque al final borras el diccionario
+	if (config_has_property(config_dictionary, "PORT")
+			&& config_has_property(config_dictionary, "ALGORITMO")
+			&& config_has_property(config_dictionary, "CANT_ENTRADAS")
+			&& config_has_property(config_dictionary, "ENTRADA_SIZE")
+			&& config_has_property(config_dictionary, "RETARDO")) {
+		strncpy(configuracion.puerto,
+				config_get_string_value(config_dictionary, "PORT"),
+				PORT_MAX_STRING_LENGTH); //hay que copiarselo porque al final borras el diccionario
 		configuracion.puerto[5] = '\0';
-		configuracion.algoritmo = get_algoritmo_distribucion(config_get_string_value(config_dictionary, "ALGORITMO"));
-		configuracion.cant_entradas = config_get_int_value(config_dictionary, "CANT_ENTRADAS");
-		configuracion.entrada_size = config_get_int_value(config_dictionary, "ENTRADA_SIZE");
-		configuracion.retardo = config_get_int_value(config_dictionary, "RETARDO");
-	}else{
+		configuracion.algoritmo = get_algoritmo_distribucion(
+				config_get_string_value(config_dictionary, "ALGORITMO"));
+		configuracion.cant_entradas = config_get_int_value(config_dictionary,
+				"CANT_ENTRADAS");
+		configuracion.entrada_size = config_get_int_value(config_dictionary,
+				"ENTRADA_SIZE");
+		configuracion.retardo = config_get_int_value(config_dictionary,
+				"RETARDO");
+	} else {
 		config_destroy(config_dictionary);
-		exit_error_with_msg("El archivo de configuracion esta incorrecto");
+		log_error(logger,
+				"El archivo de configuracion esta incorrecto");
+		log_destroy(logger);
+		exit(EXIT_FAILURE);
 	}
 
 	config_destroy(config_dictionary);
 }
 
-void limpiar_configuracion(void){
+void limpiar_configuracion(void) {
 	/*
 	 * Polimorfismo(?
 	 */
 }
 
-t_algoritmo get_algoritmo_distribucion(char* nombre_algoritmo){
-	if(string_equals_ignore_case(nombre_algoritmo, "LSU"))
+t_algoritmo get_algoritmo_distribucion(char* nombre_algoritmo) {
+	if (string_equals_ignore_case(nombre_algoritmo, "LSU"))
 		return LSU;
-	if(string_equals_ignore_case(nombre_algoritmo, "KE"))
+	if (string_equals_ignore_case(nombre_algoritmo, "KE"))
 		return KE;
-	if(string_equals_ignore_case(nombre_algoritmo, "EL"))
+	if (string_equals_ignore_case(nombre_algoritmo, "EL"))
 		return EL;
 	exit_error_with_msg("Nombre del algoritmo desconocido");
 	return -1;
